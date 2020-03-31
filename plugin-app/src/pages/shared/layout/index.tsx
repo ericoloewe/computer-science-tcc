b2bcda86248ade4934730244929c3d9bec05ae93
Erico de Souza Loewe <ed-lowe@hotmail.com>
2020-03-31 18:59:53 -0300

    add material and layout

---
+++ plugin-app/src/pages/shared/layout/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button
+} from "@material-ui/core";
+
+import { Menu } from "@material-ui/icons";
+
+interface Props {
+  children: any;
+}
+
+export function Layout({ children }: Props) {
+  return (
+    <div className="root-layout">
+      <header className="header">
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={"classes.menuButton"}
+              color="inherit"
+              aria-label="menu"
+            >
+              <Menu />
+            </IconButton>
+            <Typography variant="h6" className={"title"}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </header>
+      <article>{children}</article>
+    </div>
+  );
+}
