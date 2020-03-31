cf30355ba86526eeaee0d2d9361b9b6efc98c51c
Erico de Souza Loewe <ed-lowe@hotmail.com>
2020-03-31 19:23:40 -0300

    add flex

--- plugin-app/src/pages/welcome/index.tsx
+++ plugin-app/src/pages/welcome/index.tsx
@@ -1,20 +1,27 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./style.css";
-import { Layout } from "../shared/layout";
+import { Button, Typography } from "@material-ui/core";
 
 export function Welcome() {
   return (
-    <Layout>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Welcome
-          </p>
-          <a href="/">Go home</a>
-        </header>
-      </div>
-    </Layout>
+    <article className="welcome-page">
+      <section className="description">
+        <Typography variant="h4" component="h4">
+          Bem vindo!
+        </Typography>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </section>
+      <Button variant="contained" color="primary" href="/">
+        Continuar
+      </Button>
+    </article>
   );
 }
