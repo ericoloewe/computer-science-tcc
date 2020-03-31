fe948460067ae6fec2f0afa1946f9cd9a1908210
Erico de Souza Loewe <ed-lowe@hotmail.com>
2020-03-31 19:05:26 -0300

    add react router

--- plugin-app/src/index.tsx
+++ plugin-app/src/index.tsx
@@ -3,10 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { Home } from "./pages/home";
 import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Welcome } from "./pages/welcome";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- plugin-app/src/pages/welcome/index.tsx
+++ plugin-app/src/pages/welcome/index.tsx
@@ -3,18 +3,16 @@ import logo from "./logo.svg";
 import "./style.css";
 import { Layout } from "../shared/layout";
 
-export function Home() {
+export function Welcome() {
   return (
     <Layout>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.tsx</code> and save to reload.
+            Welcome
           </p>
-          <a className="App-link" href="/welcome">
-            Go to welcome
-          </a>
+          <a href="/">Go home</a>
         </header>
       </div>
     </Layout>
