70e27fed81f78909280fe4f2e9a007982fbf62cf
Erico de Souza Loewe <ed-lowe@hotmail.com>
2020-04-07 10:32:50 -0300

    adjust tests

--- plugin-app/src/pages/home/__tests__/index.tsx
+++ plugin-app/src/pages/home/__tests__/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import { Home } from "..";
+import Page from "..";
 
 test("renders learn react link", () => {
-  const { getByText } = render(<Home />);
+  const { getByText } = render(<Page />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
