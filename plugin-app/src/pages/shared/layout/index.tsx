2c96f6163e2cf457548335dcdf54dd0fd596a051
Erico de Souza Loewe <ed-lowe@hotmail.com>
2020-04-08 00:47:58 -0300

    add menu

--- plugin-app/src/pages/shared/layout/index.tsx
+++ plugin-app/src/pages/shared/layout/index.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
 
-import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+  ListItemAvatar,
+  Avatar,
+} from "@material-ui/core";
 
-import { Menu, MoreVert } from "@material-ui/icons";
+import { Menu, MoreVert, Home as HomeIcon, Settings as SettingsIcon } from "@material-ui/icons";
 
 interface Props {
   children: any;
@@ -13,13 +26,21 @@ interface Props {
 }
 
 export function Layout({ className, children, hideMenu, pageTitle }: Props) {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="root-layout">
       <header className="header">
         <AppBar position="static">
           <Toolbar>
             {!hideMenu && (
-              <IconButton edge="start" className={"classes.menuButton"} color="inherit" aria-label="menu">
+              <IconButton
+                edge="start"
+                className={"classes.menuButton"}
+                color="inherit"
+                aria-label="menu"
+                onClick={() => setMenuOpen(true)}
+              >
                 <Menu />
               </IconButton>
             )}
@@ -34,6 +55,29 @@ export function Layout({ className, children, hideMenu, pageTitle }: Props) {
         </AppBar>
       </header>
       <article className={className}>{children}</article>
+      <Drawer anchor="left" open={isMenuOpen} onClose={() => setMenuOpen(false)}>
+        <List component="nav" aria-label="main mailbox folders">
+          <ListItem button>
+            <ListItemAvatar>
+              <Avatar src={"https://avatars2.githubusercontent.com/u/8793862?s=400&v=4"} alt="User avatar" />
+            </ListItemAvatar>
+            <ListItemText primary="Lorem ipsum dolor set" secondary="mail@domain.com" />
+          </ListItem>
+          <Divider />
+          <ListItem button>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <SettingsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItem>
+        </List>
+      </Drawer>
     </div>
   );
 }
