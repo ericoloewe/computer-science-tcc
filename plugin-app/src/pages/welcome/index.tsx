1eef3e8331543551e131982f49941a0cdb94075e
Erico de Souza Loewe <ed-lowe@hotmail.com>
2020-04-13 22:33:59 -0300

    add welcome flow

--- plugin-app/src/pages/welcome/index.tsx
+++ plugin-app/src/pages/welcome/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.scss";
 import { Button, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 export default function () {
   return (
@@ -10,16 +11,14 @@ export default function () {
           Bem vindo!
         </Typography>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
         </p>
       </section>
-      <Button variant="contained" color="primary" href="/">
+      <Button variant="contained" color="primary" {...{ component: Link, to: `/` }}>
         Continuar
       </Button>
     </article>
