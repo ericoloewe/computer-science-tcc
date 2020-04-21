80c8888656738d5e7bbcc954e53b8eac62e68074
Erico de Souza Loewe <ed-lowe@hotmail.com>
2020-04-20 22:08:17 -0300

    add server

---
+++ plugin-app-server/web-api/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace web_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task Post()
+        {
+            _logger.LogDebug("Login OK");
+        }
+    }
+}
