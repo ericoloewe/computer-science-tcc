9ea4e5376acc90541df590b6ae2fcea16d3a0113
Erico de Souza Loewe <ed-lowe@hotmail.com>
2020-04-20 21:33:36 -0300

    add debounce

--- plugin-app/src/utils/timer.ts
+++ plugin-app/src/utils/timer.ts
@@ -1,4 +1,19 @@
 export class TimerUtil {
+  static debounce(callback: Function, wait: number): Function {
+    let timeout: NodeJS.Timeout | null;
+
+    return (...params: any[]): void => {
+      if (timeout != null) {
+        clearTimeout(timeout);
+      }
+
+      timeout = setTimeout((): void => {
+        timeout = null;
+        callback(...params);
+      }, wait);
+    };
+  }
+
   static async wait(time: number): Promise<void> {
     return new Promise((resolve) => setTimeout(() => resolve(), time));
   }
