b963199d41d11611daead554d33064d04c6d5a83
Erico de Souza Loewe <ed-lowe@hotmail.com>
2020-08-11 23:08:41 -0300

    add feelings json

--- plugin-app/src/contexts/feeling.tsx
+++ plugin-app/src/contexts/feeling.tsx
@@ -1,10 +1,11 @@
 import React, { createContext, useContext } from "react";
 import { requestService } from "../services/request";
+import { StringUtil } from "../utils/string";
 
 export interface Feeling {
   id: string;
   name: string;
-  createdDate: string;
+  createdDate?: string;
 }
 
 interface Props {}
@@ -13,6 +14,7 @@ interface Context {
   search: (text?: string) => Promise<Feeling[]>;
 }
 
+const USE_API = process.env.REACT_APP_USE_API_TO_LOAD_FEELINGS === "true";
 const apiEndpoint = process.env.REACT_APP_API_HOST || "https://localhost:44301/api";
 const eventApiEndpoint = `${apiEndpoint}/feelings`;
 
@@ -20,6 +22,21 @@ const FeelingContext = createContext({} as any);
 
 export function FeelingProvider(props: Props) {
   async function search(text?: string): Promise<Feeling[]> {
+    return USE_API ? searchFromApi(text) : searchFromJson(text);
+  }
+
+  async function searchFromJson(text?: string): Promise<Feeling[]> {
+    const { data } = await requestService.get<string[]>("/feelings.json");
+
+    return data
+      .filter((l) => !text || l.toLowerCase().includes(text?.toLowerCase()))
+      .map((l) => ({
+        id: StringUtil.toKebabCase(l),
+        name: l,
+      }));
+  }
+
+  async function searchFromApi(text?: string): Promise<Feeling[]> {
     const { data } = await requestService.get<Feeling[]>({
       url: eventApiEndpoint,
       params: { text },
