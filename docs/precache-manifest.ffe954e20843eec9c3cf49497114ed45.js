700b6a0f2255b5a087bd25045aedacabea4fc74c
Erico de Souza Loewe <ed-lowe@hotmail.com>
2020-08-14 08:55:37 -0300

    fix: update with master

---
+++ docs/precache-manifest.ffe954e20843eec9c3cf49497114ed45.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7878fdda700b9a639cd6b3e7fd56e809",
+    "url": "/computer-science-tcc/index.html"
+  },
+  {
+    "revision": "d74ab2e5fb6d5a92b989",
+    "url": "/computer-science-tcc/static/css/10.35fc799c.chunk.css"
+  },
+  {
+    "revision": "740d02f7a32745c8f966",
+    "url": "/computer-science-tcc/static/css/11.d6a6c78d.chunk.css"
+  },
+  {
+    "revision": "a574f90e313bc9aa4ae8",
+    "url": "/computer-science-tcc/static/css/12.ef088d37.chunk.css"
+  },
+  {
+    "revision": "697f265ad563f0b11850",
+    "url": "/computer-science-tcc/static/css/13.85fadd18.chunk.css"
+  },
+  {
+    "revision": "8b7747e3e539393bbce6",
+    "url": "/computer-science-tcc/static/css/14.fff01643.chunk.css"
+  },
+  {
+    "revision": "5c42107a977de46ac065",
+    "url": "/computer-science-tcc/static/css/15.6bdd9f70.chunk.css"
+  },
+  {
+    "revision": "222acc4f92454337fd93",
+    "url": "/computer-science-tcc/static/css/16.e6cdf90d.chunk.css"
+  },
+  {
+    "revision": "f72adba5a0d0504fab71",
+    "url": "/computer-science-tcc/static/css/17.ab4ae00f.chunk.css"
+  },
+  {
+    "revision": "0e171467eaa64c5fb15d",
+    "url": "/computer-science-tcc/static/css/7.29a00495.chunk.css"
+  },
+  {
+    "revision": "d4f6961b2157254bb964",
+    "url": "/computer-science-tcc/static/css/8.9f54fd29.chunk.css"
+  },
+  {
+    "revision": "4c6a207f874149ec393b",
+    "url": "/computer-science-tcc/static/css/9.7c866520.chunk.css"
+  },
+  {
+    "revision": "72368ab86130eae063c8",
+    "url": "/computer-science-tcc/static/css/main.2562c6f0.chunk.css"
+  },
+  {
+    "revision": "7a4db54c1e9bb0de2e7c",
+    "url": "/computer-science-tcc/static/js/0.d0ed8799.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/computer-science-tcc/static/js/0.d0ed8799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42900a3a5edd5febd435",
+    "url": "/computer-science-tcc/static/js/1.c84c2a92.chunk.js"
+  },
+  {
+    "revision": "d74ab2e5fb6d5a92b989",
+    "url": "/computer-science-tcc/static/js/10.0638eb09.chunk.js"
+  },
+  {
+    "revision": "740d02f7a32745c8f966",
+    "url": "/computer-science-tcc/static/js/11.435a4122.chunk.js"
+  },
+  {
+    "revision": "a574f90e313bc9aa4ae8",
+    "url": "/computer-science-tcc/static/js/12.c24a74f7.chunk.js"
+  },
+  {
+    "revision": "697f265ad563f0b11850",
+    "url": "/computer-science-tcc/static/js/13.4b21be0d.chunk.js"
+  },
+  {
+    "revision": "8b7747e3e539393bbce6",
+    "url": "/computer-science-tcc/static/js/14.c3082c3b.chunk.js"
+  },
+  {
+    "revision": "5c42107a977de46ac065",
+    "url": "/computer-science-tcc/static/js/15.43726ab8.chunk.js"
+  },
+  {
+    "revision": "222acc4f92454337fd93",
+    "url": "/computer-science-tcc/static/js/16.96bdd92e.chunk.js"
+  },
+  {
+    "revision": "f72adba5a0d0504fab71",
+    "url": "/computer-science-tcc/static/js/17.5556e39e.chunk.js"
+  },
+  {
+    "revision": "abaf79f1020da4af6797",
+    "url": "/computer-science-tcc/static/js/2.785f0ec3.chunk.js"
+  },
+  {
+    "revision": "14fb30282887f6778ee6",
+    "url": "/computer-science-tcc/static/js/5.bb864d05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/computer-science-tcc/static/js/5.bb864d05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e91253e32046f5399f46",
+    "url": "/computer-science-tcc/static/js/6.4e7858aa.chunk.js"
+  },
+  {
+    "revision": "0e171467eaa64c5fb15d",
+    "url": "/computer-science-tcc/static/js/7.a05367af.chunk.js"
+  },
+  {
+    "revision": "d4f6961b2157254bb964",
+    "url": "/computer-science-tcc/static/js/8.cbd604b0.chunk.js"
+  },
+  {
+    "revision": "4c6a207f874149ec393b",
+    "url": "/computer-science-tcc/static/js/9.0b5e8a9c.chunk.js"
+  },
+  {
+    "revision": "72368ab86130eae063c8",
+    "url": "/computer-science-tcc/static/js/main.35c5fd25.chunk.js"
+  },
+  {
+    "revision": "9ddef6d67062bff5b3d4",
+    "url": "/computer-science-tcc/static/js/runtime-main.1e3e2df7.js"
+  }
+]);
