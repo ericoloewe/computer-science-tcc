1ae6338e9fa9dc7df766384c43c617126858c625
Erico de Souza Loewe <ed-lowe@hotmail.com>
2020-08-31 01:21:27 -0300

    feat: song loader

--- plugin-app/src/components/loader/index.tsx
+++ plugin-app/src/components/loader/index.tsx
@@ -1,6 +1,12 @@
 import "./style.scss";
 import React from "react";
 
+import { ReactComponent as LoaderSvg } from "./loader.svg";
+
 export function Loader(): React.ReactElement {
-  return <div className="loader-component">Loading...</div>;
+  return (
+    <div className="loader-component">
+      <LoaderSvg />
+    </div>
+  );
 }
